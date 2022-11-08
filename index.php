afdb5e156911b0e9b46e0dcfe1bbd76b5222ce1a
Sergey Chulanov <tty8747@gmail.com>
2022-11-08 20:04:22 +0000

    Added a code to test memcached

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+echo "<hr/><center>";
+$mem = new Memcached();
+$mem->addServer("127.0.0.1", 11211);
+$result = $mem->get("blah");if ($result) {
+echo $result;
+} else {
+echo "No matching key found. I’ll add that now!";
+$mem->set("blah", "I am data! I am held in memcached!") or die("Couldn’t save anything to memcached…");
+}
+echo "<hr/>";
+echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+echo "<hr/></center>";
 phpinfo();
 ?>
